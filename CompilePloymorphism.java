ced17369e9e6847f267279fced3aef57b8620880
SwarnaMeghana <96186699+SwarnaMeghana@users.noreply.github.com>
2022-02-03 18:23:04 +0530

    Add files via upload

---
+++ CompilePloymorphism.java
@@ -0,0 +1,17 @@
+package com.dxc.java;
+
+public class CompilePloymorphism {
+    public static void main(String args[]) {
+        CompileTimePloymorphismExample obj = new CompileTimePloymorphismExample();
+        obj.display();
+        obj.display("Polymorphism");
+    }
+}
+class CompileTimePloymorphismExample {
+    void display() {
+        System.out.println("In Display without parameter");
+    }
+    void display(String value) {
+        System.out.println("In Display with parameter" + value);
+    }
+}
---
+++ MethodOverloading.java
@@ -0,0 +1,19 @@
+package com.dxc.java;
+
+class MethodOverloading {
+    private static void display(int a)
+    {
+        System.out.println(" Int data");
+    }
+
+    private static void display(String a)
+    {
+        System.out.println("String object.");
+    }
+
+    public static void main(String[] args)
+    {
+        display(5);
+        display("Hai");
+    }
+}
